5b9a493cb49ad4de40201ec25f2cf9cbe304983f
PauloSanto <tpss228@gmail.com>
2023-06-18 20:43:49 -0300

    2 players ok

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-let cont = 0
-let twop = 0
-let onep = 0
-let winn = 0
-let arpos = []
-let pos = {}
+let cont = 0        // count moves
+let twop = 0        // 2 players
+let onep = 0        // 1 player
+let winn = 0        // winner
+let arpos = []      // posicional array
+let stpc = 0        // start personal computer
+let st1p = 0        // start 1 player
+let Xor0 = 0        // X or 0
 
-function stg(){
-    let nplay = document.getElementsByName('n_play')
+function stg(){             //start game
+    let nplay = document.getElementsByName('n_play')  // nbumber players
     if(nplay[0].checked == 1){
         window.alert('You chose 1 player. If you want to change your choose, start new game. You are "X" with color yelow. Good lock.')
         document.getElementById('2prad').style.opacity = '0';
@@ -24,40 +26,43 @@ function stg(){
     }
 }
 
-function showConfirmBox() {
+function showConfirmBox() {     //overlay box
     document.getElementById("overlay").hidden = false;
   }
   function closeConfirmBox() {
     document.getElementById("overlay").hidden = true;
   }
 
-  function isConfirm(answer) {
+  function isConfirm(answer) {   
     if (answer) {
       //alert("Answer is yes");
+      st1p = 1          // start game 1 player
      
     } else {
       //alert("Answer is no");
-      playertwo()
+      stpc = 1          // start game personal computer
     }
     closeConfirmBox();
 }
 
-function qplay(np){
+function qplay(np){         //notice who play
     let txt1 = document.getElementById('txt1')
     if( np == 'X'){ 
         txt1.innerText="X"
         txt1.style.backgroundColor='yellow'
+        Xor0 = 0        // turn X
     }
     if( np == '0'){
         txt1.innerText="0"
         txt1.style.backgroundColor='green'
+        Xor0 = 1        // turn 0
     }
    document.getElementById('txt2').innerText="Sua vez de jogar"
 }
 
-function marK(id){
+function marK(id){          // html => mark
     let mark1 = document.getElementById(id);
-    if((cont < 9) && (twop == 1)) {                     //2p
+    if((cont < 9) && (twop == 1)) {                     // 2 players
         if((cont % 2 == 0) && (mark1.value == 'Z')){
             qplay('0')
                 mark1.value = 'X';
@@ -70,8 +75,8 @@ function marK(id){
                 mark1.style.color = "red";
                 mark1.style.border = 'none';
                 cont += 1;  
-                testwin() 
-    }   else if((cont < 9) && (onep == 1)) {            //1p start "X"
+                loadarr() 
+    }   else if((cont < 9) && (onep == 1)) {            // 1 player start "X"
             if(mark1.value == 'Z'){
                 qplay('0')
                 mark1.value = 'X';
@@ -79,18 +84,25 @@ function marK(id){
                 mark1.style.backgroundColor = 'yellow';
                 mark1.style.border = 'none';
                 cont += 1;
-                testwin() 
-                playertwo()
+                loadarr() 
+                cond()
+                test()
             } 
-    } else if((cont < 9) && (winn == 0)){
-            window.alert('click ON Start Game')
-            }
-    if((cont == 9) && (winn == 0)){ tie()}      
+    }  else if((cont < 9) && (winn == 0)){          // notice about game
+        window.alert('click ON Start Game')
+    }
+    cond() 
+}
+function cond(){            // conditionals
+    //if((cont > 3) && (onep == 1))  { test()}  // start X check position
+    if(cont > 4) {testwin()}                  // test if exist winner   
+    if((cont == 9) && (winn == 0)){ tie()}    // test final play
+
 }
 
 function playertwo(){
         let mark2 = document.getElementById('pos'+ randomInteger()); 
-        if(cont == 0){                          // first move init game
+        if ((onep == 1) && (winn == 0)){                     // first move start pc
             if (mark2.value == "Z") {
                 qplay('X')
                 mark2.value = '0';
@@ -98,36 +110,11 @@ function playertwo(){
                 mark2.style.color = "red";
                 mark2.style.border = 'none';
                 cont += 1;  
-                testwin()
+                loadarr()
                 return;
             }   else{ playertwo()}
         }
-
-        if(cont == 1){                          // first move
-            if (mark2.value == "Z") {
-                qplay('X')
-                mark2.value = '0';
-                mark2.style.backgroundColor = 'green';
-                mark2.style.color = "red";
-                mark2.style.border = 'none';
-                cont += 1;  
-                testwin()
-                return;
-            }   else{ playertwo()}
-        }   
-
-        if((cont > 1) && (cont < 9) && (winn == 0)){
-            if (mark2.value == "Z") {
-                qplay('X');
-                mark2.value = '0';
-                mark2.style.backgroundColor = 'green';
-                mark2.style.color = "red";
-                mark2.style.border = 'none';
-                cont += 1;  
-                testwin()
-                return;
-            }   else{ playertwo()}
-        }   
+  
 }
     
         //if(arpos.filter (x => x =='X').length == 1) { win('X', 1, 2, 3)}
@@ -136,15 +123,17 @@ function randomInteger() {
         return Math.floor(Math.random() * 9) + 1;
 }
 
-function testwin(){
+function loadarr(){
     for(let i = 1; i < 10; i++){
-        arpos[i] = document.getElementById(['pos'+i]).value
-        
-    }
-
-    if([arpos[1], arpos[2], arpos[3]].filter (x => x =='X').length == 3) { win('X', 1, 2, 3)}
-    if([arpos[4], arpos[5], arpos[6]].filter (x => x =='X').length == 3) { win('X', 4, 5, 6)}
-    if([arpos[7], arpos[8], arpos[9]].filter (x => x =='X').length == 3) { win('X', 7, 8, 9)}
+    arpos[i] = document.getElementById('pos' + i).value
+}
+}
+function testwin(){
+    loadarr()
+    
+    if( arpos.slice(1,4).filter (x => x =='X').length == 3) { win('X', 1, 2, 3)}
+    if( arpos.slice(4,7).filter (x => x =='X').length == 3) { win('X', 4, 5, 6)}
+    if( arpos.slice(7,10).filter (x => x =='X').length == 3) { win('X', 7, 8, 9)}
     if([arpos[1], arpos[4], arpos[7]].filter (x => x =='X').length == 3) { win('X', 1, 4, 7)}
     if([arpos[2], arpos[5], arpos[8]].filter (x => x =='X').length == 3) { win('X', 2, 5, 8)}
     if([arpos[3], arpos[6], arpos[9]].filter (x => x =='X').length == 3) { win('X', 3, 6, 9)}
@@ -159,10 +148,47 @@ function testwin(){
     if([arpos[3], arpos[6], arpos[9]].filter (x => x =='0').length == 3) { win('0', 3, 6, 9)}
     if([arpos[1], arpos[5], arpos[9]].filter (x => x =='0').length == 3) { win('0', 1, 5, 9)}
     if([arpos[3], arpos[5], arpos[7]].filter (x => x =='0').length == 3) { win('0', 3, 5, 7)}
-    
+}
 
-  
+function test(){
     
+    if (winn == 0){
+        if(([arpos[1], arpos[2], arpos[3]].filter (x => x =='0').length == 2)
+        && (Xor0 == 1)) { movpc('0', 1, 2, 3)}
+        if(([arpos[4], arpos[5], arpos[6]].filter (x => x =='0').length == 2)
+            && (Xor0 == 1)) { movpc('0', 4, 5, 6)}
+        if(([arpos[7], arpos[8], arpos[9]].filter (x => x =='0').length == 2)
+            && (Xor0 == 1)) { movpc('0', 7, 8, 9)}
+        if(([arpos[1], arpos[4], arpos[7]].filter (x => x =='0').length == 2)
+            && (Xor0 == 1)) { movpc('0', 1, 4, 7)}
+        if(([arpos[2], arpos[5], arpos[8]].filter (x => x =='0').length == 2)
+            && (Xor0 == 1)) { movpc('0', 2, 5, 8)}
+        if(([arpos[3], arpos[6], arpos[9]].filter (x => x =='0').length == 2)
+            && (Xor0 == 1)) { movpc('0', 3, 6, 9)}
+        if(([arpos[1], arpos[5], arpos[9]].filter (x => x =='0').length == 2)
+            && (Xor0 == 1)) { movpc('0', 1, 5, 9)}
+        if(([arpos[3], arpos[5], arpos[7]].filter (x => x =='0').length == 2)
+            && (Xor0 == 1)) { movpc('0', 3, 5, 7)}
+
+        if((arpos.slice(1,4).filter (x => x =='X').length == 2) 
+           && (Xor0 == 1)) { movpc('X', 1, 2, 3)}
+        if(( arpos.slice(4,7).filter (x => x =='X').length == 2) 
+           && (Xor0 == 1)) { movpc('X', 4, 5, 6)}
+        if(( arpos.slice(7,10).filter (x => x =='X').length == 2)
+           && (Xor0 == 1)){ movpc('X', 7, 8, 9)}
+        if(([arpos[1], arpos[4], arpos[7]].filter (x => x =='X').length == 2)
+           && (Xor0 == 1)) { movpc('X', 1, 4, 7)}
+        if(([arpos[2], arpos[5], arpos[8]].filter (x => x =='X').length == 2) 
+           && (Xor0 == 1)){ movpc('X', 2, 5, 8)}
+        if(([arpos[3], arpos[6], arpos[9]].filter (x => x =='X').length == 2) 
+           && (Xor0 == 1)){ movpc('X', 3, 6, 9)}
+        if(([arpos[1], arpos[5], arpos[9]].filter (x => x =='X').length == 2)
+            && (Xor0 == 1)) { movpc('X', 1, 5, 9)}
+        if([arpos[3], arpos[5], arpos[7]].filter (x => x =='X').length == 2) { movpc('X', 3, 5, 7)}
+    
+        if( Xor0 == 1) { playertwo()}
+        cond()
+    }
 }
 
 function win(w, n1, n2, n3){
@@ -173,13 +199,105 @@ function win(w, n1, n2, n3){
     txt1.innerText = w
     if(w == 'X'){  txt1.style.backgroundColor='yellow'}
     if(w == '0'){  txt1.style.backgroundColor='green'}   
-    document.getElementById('txt2').innerText="VOCE VENCEU!!!"
+    document.getElementById('txt2').innerHTML="VOCE VENCEU!!!"
     document.getElementById(posA).style.border = '6px solid black'
     document.getElementById(posB).style.border = '6px solid black'
     document.getElementById(posC).style.border = '6px solid black'
+    win_img()
     onep = 0
     twop = 0
     winn = 1
+}
+function win_img(){
+   
+     document.getElementById("container").style.backgroundImage =  "url('img/TROFEU.png')"
+}
+
+function movpc(w, n1, n2, n3){
+    loadarr()
+    if((w == '0') && (winn == 0) && (Xor0 == 0)){
+        if(arpos[n1] == 'Z') {
+                let mark3 = document.getElementById('pos'+ n1) 
+                qplay('X')
+                mark3.value = '0';
+                mark3.style.backgroundColor = 'green';
+                mark3.style.color = "red";
+                mark3.style.border = 'none';
+                loadarr()
+                cont += 1;  
+                Xor0 = 0
+             
+            } 
+        if(arpos[n2] == 'Z') {
+                let mark3 = document.getElementById('pos'+ n2) 
+                qplay('X')
+                mark3.value = '0';
+                mark3.style.backgroundColor = 'green';
+                mark3.style.color = "red";
+                mark3.style.border = 'none';
+                loadarr()
+                cont += 1;  
+                Xor0 = 0
+               
+            } 
+        if(arpos[n3] == 'Z') { 
+                let mark3 = document.getElementById('pos'+ n3) 
+                qplay('X')
+                mark3.value = '0';
+                mark3.style.backgroundColor = 'green';
+                mark3.style.color = "red";
+                mark3.style.border = 'none';
+                loadarr()
+                cont += 1;  
+                Xor0 = 0
+               
+        } 
+        
+    }
+
+    if((w == 'X') && (winn == 0) && (Xor0 == 0)){
+        if(arpos[n1] == 'Z') {
+                let mark3 = document.getElementById('pos'+ n1) 
+                qplay('X')
+                mark3.value = '0';
+                mark3.style.backgroundColor = 'green';
+                mark3.style.color = "red";
+                mark3.style.border = 'none';
+                loadarr()
+                cont += 1;  
+                Xor0 = 0
+               
+            } 
+        if(arpos[n2] == 'Z') {
+                let mark3 = document.getElementById('pos'+ n2) 
+                qplay('X')
+                mark3.value = '0';
+                mark3.style.backgroundColor = 'green';
+                mark3.style.color = "red";
+                mark3.style.border = 'none';
+                loadarr()
+                cont += 1;  
+                Xor0 = 0
+               
+            } 
+        if(arpos[n3] == 'Z') { 
+                let mark3 = document.getElementById('pos'+ n3) 
+                qplay('X')
+                mark3.value = '0';
+                mark3.style.backgroundColor = 'green';
+                mark3.style.color = "red";
+                mark3.style.border = 'none';
+                loadarr()
+                cont += 1;  
+                Xor0 = 0
+               
+        } 
+        
+    }
+  
+      
+
+
 }
 
 function tie(){
@@ -191,46 +309,39 @@ function tie(){
 function ng(){
     window.location.reload();
 }
+
 /*
-      if((cont % 2 != 0) && (mark1.value == 'Z')){
-                    qplay('X')
-                    mark1.value = '0';
-                    mark1.style.color = "red";
-                    mark1.style.backgroundColor = 'green';
-                    mark1.style.border = 'none';
-                    cont += 1;
-                }
-                mark1.style.color = "red";
-                mark1.style.border = 'none';
+IA game defense
+   
+
+    if([arpos[1], arpos[2], arpos[3]].filter (x => x =='X').length == 2) { defense()}
+    if([arpos[4], arpos[5], arpos[6]].filter (x => x =='X').length == 2) { win('X', 4, 5, 6)}
+    if([arpos[7], arpos[8], arpos[9]].filter (x => x =='X').length == 2) { win('X', 7, 8, 9)}
+    if([arpos[1], arpos[4], arpos[7]].filter (x => x =='X').length == 2) { win('X', 1, 4, 7)}
+    if([arpos[2], arpos[5], arpos[8]].filter (x => x =='X').length == 2) { win('X', 2, 5, 8)}
+    if([arpos[3], arpos[6], arpos[9]].filter (x => x =='X').length == 2) { win('X', 3, 6, 9)}
+    if([arpos[1], arpos[5], arpos[9]].filter (x => x =='X').length == 2) { win('X', 1, 5, 9)}
+    if([arpos[3], arpos[5], arpos[7]].filter (x => x =='X').length == 2) { win('X', 3, 5, 7)}
+
+
+
+        if((cont == 1) && (st1p == 1) && (onep == 1)){                          // first move start 1p
+            if (mark2.value == "Z") {
+                qplay('X')
+                mark2.value = '0';
+                mark2.style.backgroundColor = 'green';
+                mark2.style.color = "red";
+                mark2.style.border = 'none';
                 cont += 1;  
-                testwin()
+                loadarr()
+                return;
+            }   else{ playertwo()}
+        }   
 
-    if([pos.pos1, pos.pos2, pos.pos3].filter(x => x=='X').length == 3) { win('X', 1, 2, 3)}
-    arpos[i] = document.getElementById(['pos'+i]).value
-
-    pos.pos1 = document.getElementById('pos1').value
-    pos.pos2 = document.getElementById('pos2').value
-    pos.pos3 = document.getElementById('pos3').value
-    pos.pos4 = document.getElementById('pos4').value
-    pos.pos5 = document.getElementById('pos5').value
-    pos.pos6 = document.getElementById('pos6').value
-    pos.pos7 = document.getElementById('pos7').value
-    pos.pos8 = document.getElementById('pos8').value
-    pos.pos9 = document.getElementById('pos9').value
-    
-    
-// IA game
-function pos(){
-    let ar1 = [ pos.pos1, pos.pos2, pos.pos3 ]
-    let ar2 = [ pos.pos4, pos.pos5, pos.pos6 ]
-    let ar3 = [ pos.pos7, pos.pos8, pos.pos9 ]
-    let ar4 = [ pos.pos1, pos.pos4, pos.pos7 ]
-    let ar5 = [ pos.pos2, pos.pos5, pos.pos8 ]
-    let ar6 = [ pos.pos3, pos.pos6, pos.pos9 ]
-    let ar7 = [ pos.pos1, pos.pos5, pos.pos9 ]
-    let ar8 = [ pos.pos3, pos.pos5, pos.pos7 ] 
-    ar1.count('X')
-    ar1.filter(x => x=='X').length
-   if( [pos.pos1, pos.pos2, pos.pos3].filter(x => x=='X').length == 3) { win('X', 1, 2, 3)}
-}
+        if((cont > 1) && (cont < 9) && (winn == 0) && (onep == 1) && (Xor0 == 1)){
+            
+               
+                test()
+          
+        } 
 */
